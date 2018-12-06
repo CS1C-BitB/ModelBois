db9f4673e1ddd30f9d9809f85ae463d14c5e179a
Tucker Harvey <tharvey5@saddleback.edu>
2018-12-06 00:09:42 -0800

    updated comments of all shape derived classes

--- src/Ellipse.h
+++ src/Ellipse.h
@@ -3,26 +3,100 @@
 
 #include "Shape.h"
 
+/**
+ * @brief An Ellipse class.
+ *
+ * This class represents an ellipse, which is derived from shape.
+ * Please note -- a circle is an ellipse, any object which represents a circle should
+ * use the ellipse class
+ */
 class Ellipse : public Shape
 {
 public:
+    /**
+     * @brief Constructor
+     * @param width sets the horzontal distance of the ellipse
+     * @param height sets the vertical distance of the ellipse
+     * @param pos sets the location of shape placement in x, y plane
+     * @param brush sets the brush properties for the ellipse
+     * @param pen sets the pen properties for the ellipse
+     * @param id sets the id for the specific ellipse
+     *
+     * Please note that the major axis of an ellipse is always the axis will the greatest length and the minor axis is the
+     * axis with the smallest length. Minor/Major axis is determined by size in relation to each other
+     */
 	Ellipse(int width = 0, int height = 0, const QPoint& pos = {}, const QBrush &brush = {}, const QPen &pen = {}, id_t id = 0);
-	Ellipse(Ellipse &&move) noexcept;
+
+    /**
+     * @brief Move Constructor
+     * @param move Takes in a rvalue using an rvalue reference
+     */
+    Ellipse(Ellipse &&move) noexcept;
+
+    /**
+     *@brief Destructor
+     */
 	~Ellipse() override;
 	
+    /**
+     * @brief operator = Move assignment
+     * @param move, updates current object with data referred to by rhs rvalue
+     * @return reference to the current object
+     */
 	Ellipse& operator=(Ellipse &&move) noexcept;
 	
+    /**
+     * @brief draw displays the shape and is placed in an x y plane
+     * @param device, takes in a qpaint device
+     */
 	void draw(QPaintDevice* device) override;
 	
-	ShapeType getType() const override;
+    /**
+     * @brief getType Gets the type of ellipse
+     * @return The shapeType
+     */
+    ShapeType getType() const override;
+
+    /**
+     * @brief getPerimeter Gets the perimeter of the ellipse.
+     * @return The perimeter, or a negative value if shape has no perimeter.
+     */
 	double getPerimeter() const override;
+
+    /**
+     * @brief getArea Gets the area of the ellipse.
+     * @return The area, or a negative value if shape has no area.
+     */
 	double getArea() const override;
+
+    /**
+     * @brief getRect Gets the bounding rectangle of the ellipse.
+     * @return Bounding rectangle.
+     */
 	QRect getRect() const override;
 	
+    /**
+     * @brief getWidth Gets the width of the ellipse
+     * @return  width
+     */
 	int getWidth() const;
+
+    /**
+     * @brief getHeight Gets the height of the ellipse
+     * @return height
+     */
 	int getHeight() const;
 	
+    /**
+     * @brief setWidth sets the width of ellipse
+     * @param w value used to set the width
+     */
 	void setWidth(int w);
+
+    /**
+     * @brief setHeight sets the height of the ellipse
+     * @param h value used to set the height
+     */
 	void setHeight(int h);
 	
 private:
--- src/Shape.h
+++ src/Shape.h
@@ -8,14 +8,21 @@
 
 #include <memory>
 
+/**
+ * \enum ShapeType
+ * @brief The ShapeType enum.
+ *
+ * This enum contains the basic shape types that can be derived from using this class
+ */
 enum ShapeType {
-	LineType,
-	PolylineType,
-	PolygonType,
-	RectangleType,
-	EllipseType,
-	TextType
+    LineType,        /**< \var enum value LineType. */
+    PolylineType,    /**< \var enum value PolylineType. */
+    PolygonType,     /**< \var enum value PolygonType. */
+    RectangleType,   /**< \var enum value RectangleType. */
+    EllipseType,     /**< \var enum value EllipseType. */
+    TextType         /**< \var enum value TextType. */
 };
+
 
 extern const QMap<QString, QColor> COLOR_NAMES;
 extern const QMap<ShapeType, QString> SHAPE_NAMES;
@@ -24,88 +31,202 @@ extern const QMap<Qt::PenStyle, QString> PEN_STYLE_NAMES;
 extern const QMap<Qt::PenCapStyle, QString> PEN_CAP_STYLE_NAMES;
 extern const QMap<Qt::PenJoinStyle, QString> PEN_JOIN_STYLE_NAMES;
 
+/**
+ * @brief A Shape class.
+ *
+ * This is an abstract base class in which all shapes get their basic functionality
+ */
 class Shape
 {
 public:
-	using id_t = unsigned int;
-	
-	Shape(const QPoint &pos = {}, const QBrush &brush = {}, QPen pen = {}, id_t id = 0);
-	Shape(const Shape&) = delete;
-	virtual ~Shape();
-	
-	Shape& operator=(const Shape&) = delete;
-	
-	bool operator==(const Shape &other) const;
-	bool operator<(const Shape &other) const;
-	
-	virtual void draw(QPaintDevice* device) = 0;
-	/**
-	 * @brief move Moves the shape by a given offset.
-	 * @param dx Change in x.
-	 * @param dy Change in y.
-	 */
-	void move(int dx, int dy);
-	
-	virtual ShapeType getType() const = 0;
-	/**
-	 * @brief getPerimeter Gets the perimeter of the shape.
-	 * @return The perimeter, or a negative value if shape has no perimeter.
-	 */
-	virtual double getPerimeter() const = 0;
-	/**
-	 * @brief getArea Gets the area of the shape.
-	 * @return The area, or a negative value if shape has no area.
-	 */
-	virtual double getArea() const = 0;
-	
-	/**
-	 * @brief getRect Gets the bounding rectangle of the shape.
-	 * @return Bounding rectangle.
-	 */
-	virtual QRect getRect() const = 0;
-	
-	/**
-	 * @brief getPos Gets the position of the shape.
-	 * @return Position
-	 * 
-	 * For point-based shapes (line, polygon, polyline), position is the center of the shape.
-	 */
-	const QPoint& getPos() const;
-	const QBrush& getBrush() const;
-	const QPen& getPen() const;
-	
-	id_t getID() const;
-	
-	/**
-	 * @brief setPos Sets the shape's position.
-	 * @param x New x.
-	 * @param y New y.
-	 */
-	void setPos(int x, int y);
-	/**
-	 * @brief setPos Sets the shape's position.
-	 * @param pos New position.
-	 */
-	void setPos(const QPoint& pos);
-	void setBrush(const QBrush&);
-	void setPen(const QPen&);
-	
-	static bool hasFill(ShapeType);
-	
+    /**
+     * @brief id_t renames unsigned ints
+     *
+     * This name is used for any variable which is defined as an unsigned int
+     */
+    using id_t = unsigned int;
+
+    /**
+     * @brief Shape Constructor.
+     * @param pos location of your shape
+     * @param brush used to store brush properties in the shape
+     * @param pen used to store pen properties in the shape
+     * @param id used to store the id of the shape
+     *
+     */
+    Shape(const QPoint &pos = {}, const QBrush &brush = {}, QPen pen = {}, id_t id = 0);
+
+    /**
+     * @brief Shape Copy Constructor.
+     * @param Shape The copy constructor requires that it receives a shape
+     */
+    Shape(const Shape&) = delete;
+
+    /**
+     * @brief A Destructor.
+     * This is the destructor for the abstract base class shape
+     */
+    virtual ~Shape();
+
+    /**
+     * @brief operator = overloaded assignment operator
+     * @param pass in another shape object to make a copy to the current shape
+     * @return a reference to the current shape
+     */
+    Shape& operator=(const Shape&) = delete;
+
+    /**
+     * @brief operator == Checks for the equality between two shape ids
+     * @param other Used to compare the id of another shape against the current shape
+     * @return a bool, true if the ids are equal
+     */
+    bool operator==(const Shape &other) const;
+
+    /**
+     * @brief operator < Checks the relationship between two ids
+     * @param other Used to check if the current id is smaller than the id on the right hand side of the operand
+     * @return a bool, true if the current id is less than the id to the right of the operand
+     */
+    bool operator<(const Shape &other) const;
+
+    /**
+     * @brief draw Is a pure virtual function in the shape class
+     * @param device Receives a QPaintDevice
+     */
+    virtual void draw(QPaintDevice* device) = 0;
+
+    /**
+     * @brief move Moves the shape by a given offset.
+     * @param dx Change in x.
+     * @param dy Change in y.
+     */
+    void move(int dx, int dy);
+
+    /**
+     * @brief getType Gets the type of shape
+     * @return The shapeType
+     */
+    virtual ShapeType getType() const = 0;
+
+    /**
+     * @brief getPerimeter Gets the perimeter of the shape.
+     * @return The perimeter, or a negative value if shape has no perimeter.
+     */
+    virtual double getPerimeter() const = 0;
+
+    /**
+     * @brief getArea Gets the area of the shape.
+     * @return The area, or a negative value if shape has no area.
+     */
+    virtual double getArea() const = 0;
+
+    /**
+     * @brief getRect Gets the bounding rectangle of the shape.
+     * @return Bounding rectangle.
+     */
+    virtual QRect getRect() const = 0;
+
+    /**
+     * @brief getPos Gets the position of the shape.
+     * @return Position
+     *
+     * For point-based shapes (line, polygon, polyline), position is the center of the shape.
+     */
+    const QPoint& getPos() const;
+
+    /**
+     * @brief getBrush Gets the brush information stored in the shape
+     * @return brush information
+     */
+    const QBrush& getBrush() const;
+
+    /**
+     * @brief getPen Gets any pen information that is stored in the shape
+     * @return qpen information
+     */
+    const QPen& getPen() const;
+
+    /**
+     * @brief getID gets the shape id
+     * @return shape id
+     */
+    id_t getID() const;
+
+    /**
+     * @brief setPos Sets the shape's position.
+     * @param x New x.
+     * @param y New y.
+     */
+    void setPos(int x, int y);
+    /**
+     * @brief setPos Sets the shape's position.
+     * @param pos New position.
+     */
+    void setPos(const QPoint& pos);
+
+    /**
+     * @brief setBrush allows you to set relevant brush information for your shape
+     * @param a qbrush
+     */
+    void setBrush(const QBrush&);
+
+    /**
+     * @brief setPen allows you to set relevant pen information for your shape
+     * @param a qpen
+     */
+    void setPen(const QPen&);
+
+    /**
+     * @brief hasFill, checks that the shape is not a line, polyline, or text object
+     * @return bool, true if the current shape can contain a fill
+     *
+     * Only non-point based shapes such as Ellipse, Polygon, or Rectangle can contain any type of fill
+     */
+    static bool hasFill(ShapeType);
+
 protected:
-	Shape(id_t id);
-	void swap(Shape &other) noexcept;
-	std::unique_ptr<QPainter> getPainter(QPaintDevice* device) const;
-	
+    /**
+     * @brief Overloaded constructor that takes in shape id
+     * @param id
+     */
+    Shape(id_t id);
+
+    /**
+     * @brief swap The position, brush, pen and id of two shapes
+     * @param other, shape to swap
+     */
+    void swap(Shape &other) noexcept;
+
+    /**
+     * @brief getPainter
+     * @param device receives the a qpaint device
+     * @return a smart pointer of the qpainter
+     */
+    std::unique_ptr<QPainter> getPainter(QPaintDevice* device) const;
+
 private:
-	QPoint pos;
-	QBrush brush;
-	QPen pen;
-	
-	static QPen id_pen;
-	static QFont id_font;
-	
-	id_t id{0};
+    /**
+     * @brief pos, stores the position of the shape
+     */
+    QPoint pos;
+
+    /**
+     * @brief brush, stores the brush information of the shape
+     */
+    QBrush brush;
+
+    /**
+     * @brief stores the pen information of the shape
+     */
+    QPen pen;
+
+    static QPen id_pen;
+    static QFont id_font;
+
+    /**
+     * @brief id stores the shape id
+     */
+    id_t id{0};
 };
 
 #endif // SHAPE_H
